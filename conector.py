797ae57b43afa8ef9d3c5262cba79d59e48885ab
Marlon Ruiz <marlon.ruiz@qpaypro.com>
2026-02-03 09:41:55 -0600

    cambios en el socket

--- conector.py
+++ conector.py
@@ -7,13 +7,16 @@ load_dotenv()
 
 def create_connection():
     try:
-        connection = mysql.connector.connect(
-            host=os.getenv("DB_HOST"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-            database=os.getenv("DB_DATABASE"),
-            #unix_socket=os.getenv("DB_SOCKET")
-        )
+        config = {
+            "host": os.getenv("DB_HOST"),
+            "user": os.getenv("DB_USER"),
+            "password": os.getenv("DB_PASSWORD"),
+            "database": os.getenv("DB_DATABASE"),
+        }
+        socket_path = os.getenv("DB_SOCKET")
+        if socket_path:
+            config["unix_socket"] = socket_path
+        connection = mysql.connector.connect(**config)
         if connection.is_connected():
             print(f" Conexión exitosa a la base de datos {os.getenv('DB_DATABASE')}")
             return connection
